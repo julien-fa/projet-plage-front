d5f179ed45c826d8159b6af7498c2b9fe0b4f521
julien-fa <julien.farre@gmail.com>
2023-01-30 22:35:51 +0100

    validation des données du formulaire

--- src/app/inscription/inscription/inscription.component.ts
+++ src/app/inscription/inscription/inscription.component.ts
@@ -6,6 +6,7 @@ import { Router } from '@angular/router';
 import { Pays } from 'src/app/model/Pays';
 import { PaysService } from 'src/app/services/pays.service';
 import { Locataire } from 'src/app/model/Locataire';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-inscription',
@@ -15,6 +16,16 @@ import { Locataire } from 'src/app/model/Locataire';
 export class InscriptionComponent {
   //attributs
   listePays: Pays[] = [];
+  erreurnom: string = '';
+  erreurprenom: string = '';
+  erreuremail: string = '';
+  erreurpays: string = '';
+  erreurpassword: string = '';
+  erreurlienparente: string = '';
+  errormessage: string = '';
+
+  //regex email
+  regex = new RegExp('[a-z0-9-.]+@[a-z]+.[a-z]{2,3}');
 
   constructor(
     private paysService: PaysService,
@@ -44,26 +55,51 @@ export class InscriptionComponent {
   verificationValidations(formLogin: NgForm) {
     let infos = formLogin.value;
     console.log(infos);
-    if(infos.nom == "" || infos.nom == null){
-      
+
+    if (infos.nom == '' || infos.nom == null) {
+      this.erreurnom = 'Le champ nom est vide ou invalide';
+    } else if (infos.prenom == '' || infos.prenom == null) {
+      this.erreurprenom = 'Le champ prénom est vide ou invalide';
+    } else if (
+      infos.email == '' ||
+      infos.email == null ||
+      this.regex.test(infos.email) == false
+    ) {
+      this.erreuremail = 'Le champ email est vide ou invalide';
+    } else if (
+      infos.password == '' ||
+      infos.password == null ||
+      infos.password.length < 8
+    ) {
+      this.erreurpassword =
+        'Le champ mot de passe est vide, ne contient pas au moins 8 caractères ou il est invalide';
+    } else if (infos.pays == '' || infos.pays == null) {
+      this.erreurpays = 'Le pays est invalide';
+    } else if (infos.lienDeParente == '' || infos.lienDeParente == null) {
+      this.erreurlienparente = 'Le champ lien de parenté est invalide';
+    } else {
+      let locataireValide = new Locataire(
+        infos.nom,
+        infos.prenom,
+        infos.email,
+        infos.password,
+        infos.pays,
+        infos.lienDeParente
+      );
+      //console.log(locataireValide);
+      this.locataireService.ajouterLocataire(locataireValide).subscribe({
+        next: () => this.router.navigate(['']),
+        error: (err) => this.gestionErreurs(err.error),
+      });
     }
-    let locataireValide = new Locataire(
-      infos.nom,
-      infos.prenom,
-      infos.email,
-      infos.password,
-      infos.pays,
-      infos.lienDeParente
-    );
-    console.log(locataireValide);
-    this.locataireService.ajouterLocataire(locataireValide).subscribe({
-      next: () => this.router.navigate(['']),
-    });
+  }
+
+  //gestion des erreurs
+  gestionErreurs(message: string): void {
+    this.errormessage = message;
   }
 
   //méthode de redirection
-  redirectionFormulaireInscription(){
-
-  }
+  redirectionFormulaireInscription() {}
   redirectionAccueil() {}
 }
